6a23427119fd4ceca6541b8060815a31a3e77f95
AlexIsaev11022007 <148742956+AlexIsaev11022007@users.noreply.github.com>
2023-12-12 12:56:46 +0500

    форма html для 2 задачи

---
+++ script.js
@@ -0,0 +1,16 @@
+function convertMileToMeter() {
+    // Получаем значение введенное пользователем
+    var mileInput = document.getElementById("mile-input").value;
+
+    // Проверяем, что пользователь ввел число
+    if (isNaN(mileInput)) {
+        alert("Пожалуйста, введите числовое значение.");
+        return;
+    }
+
+    // Вычисляем расстояние в метрах
+    var meter = mileInput * 1852;
+
+    // Выводим результат
+    document.getElementById("result").innerHTML = mileInput + " миль = " + meter + " метров.";
+}
